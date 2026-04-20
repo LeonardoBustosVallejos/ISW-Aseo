6eb0652d0db3862a5044233f9b9be76c8a9cea2a
Leonardo Emilio Bustos Vallejos <103396502+LeonardoBustosVallejos@users.noreply.github.com>
2026-04-20 15:44:11 -0400

    Update InitialSetUp.js

--- backend/src/config/initialSetup.js
+++ backend/src/config/initialSetup.js
@@ -15,7 +15,7 @@ async function createUsers() {
         userRepository.create({
           nombreCompleto: "Diego Alexis Salazar Jara",
           rut: "21.308.770-3",
-          email: "administrador2024@gmail.cl",
+          email: "administrador@gmail.cl",
           password: await encryptPassword("admin1234"),
           rol: "administrador",
         }),
@@ -24,7 +24,7 @@ async function createUsers() {
         userRepository.create({
           nombreCompleto: "Diego Sebastián Ampuero Belmar",
           rut: "21.151.897-9",
-          email: "usuario1.2024@gmail.cl",
+          email: "usuario1@gmail.cl",
           password: await encryptPassword("user1234"),
           rol: "usuario",
         })
@@ -33,7 +33,7 @@ async function createUsers() {
           userRepository.create({
             nombreCompleto: "Alexander Benjamín Marcelo Carrasco Fuentes",
             rut: "20.630.735-8",
-            email: "usuario2.2024@gmail.cl",
+            email: "usuario2@gmail.cl",
             password: await encryptPassword("user1234"),
             rol: "usuario",
           }),
@@ -42,7 +42,7 @@ async function createUsers() {
         userRepository.create({
           nombreCompleto: "Pablo Andrés Castillo Fernández",
           rut: "20.738.450-K",
-          email: "usuario3.2024@gmail.cl",
+          email: "usuario3@gmail.cl",
           password: await encryptPassword("user1234"),
           rol: "usuario",
         }),
@@ -51,7 +51,7 @@ async function createUsers() {
         userRepository.create({
           nombreCompleto: "Felipe Andrés Henríquez Zapata",
           rut: "20.976.635-3",
-          email: "usuario4.2024@gmail.cl",
+          email: "usuario4@gmail.cl",
           password: await encryptPassword("user1234"),
           rol: "usuario",
         }),
@@ -60,7 +60,7 @@ async function createUsers() {
         userRepository.create({
           nombreCompleto: "Diego Alexis Meza Ortega",
           rut: "21.172.447-1",
-          email: "usuario5.2024@gmail.cl",
+          email: "usuario5@gmail.cl",
           password: await encryptPassword("user1234"),
           rol: "usuario",
         }),
@@ -69,7 +69,7 @@ async function createUsers() {
         userRepository.create({
           nombreCompleto: "Juan Pablo Rosas Martin",
           rut: "20.738.415-1",
-          email: "usuario6.2024@gmail.cl",
+          email: "usuario6@gmail.cl",
           password: await encryptPassword("user1234"),
           rol: "usuario",
         }),
