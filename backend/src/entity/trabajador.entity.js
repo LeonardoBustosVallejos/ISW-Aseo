43e2893189b850f5b5238221de13b8620eb6c5c7
Leonardo Emilio Bustos Vallejos <103396502+LeonardoBustosVallejos@users.noreply.github.com>
2026-04-26 22:59:11 -0400

    New: trabajador.entity.js
    - Se creó la entidad trabajador, de esta manera podrá crearse trabajadores y editar a posterior.
    - Se debe hacer que la entidad pueda tener imagenes y pdf, pero como no se ha pasado eso en las ayudantías se hará posteriormente.
    - La columna "competencias" podrá cambiar a futuro, ya que hay que coordinar mejor con el compañero encargado de los items que manejará la empresa del product owner.

---
+++ backend/src/entity/trabajador.entity.js
@@ -0,0 +1,58 @@
+"use strict";
+import { EntitySchema } from "typeorm";
+
+const TrabajadorSchema = new EntitySchema({
+  name: "Trabajador",
+  tableName: "trabajadores",
+  columns: {
+    id: {
+      type: "int",
+      primary: true,
+      generated: true,
+    },
+    nombreCompleto: {
+      type: "varchar",
+      length: 225,
+      nullable: false,
+    },
+    rut: {
+      type: "varchar",
+      length: 12,
+      nullable: false,
+      unique: true,
+    },
+    nacimiento: {
+      type: "date",
+      nullable: false,
+    },
+    email: {
+      type: "varchar",
+      length: 255,
+      nullable: false,
+      unique: true,
+    },
+    rol: {
+      type: "varchar",
+      length: 255,
+      nullable: false,
+    },
+    competencias: {
+      type: "varchar",
+      length: 255,
+      nullable: true,
+    },
+    createdAt: {
+      type: "timestamp with time zone",
+      default: () => "CURRENT_TIMESTAMP",
+      nullable: false,
+    },
+    updatedAt: {
+      type: "timestamp with time zone",
+      default: () => "CURRENT_TIMESTAMP",
+      onUpdate: "CURRENT_TIMESTAMP",
+      nullable: false,
+    },
+  },
+});
+
+export default TrabajadorSchema;
