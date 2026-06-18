76fc4c149a108ff680153c41234d7c5caf437254
Leonardo Emilio Bustos Vallejos <103396502+LeonardoBustosVallejos@users.noreply.github.com>
2026-06-17 20:16:45 -0400

    Update: AsignarTrabajador.jsx
    FRONTEND:
    - Update: Ahora se observan todos los datos del trabajador como corresponde, se limpiará el backend para que aparezcan mas pulidos.

--- frontend/src/pages/trabajadores/AsignarTrabajador.jsx
+++ frontend/src/pages/trabajadores/AsignarTrabajador.jsx
@@ -17,15 +17,15 @@ export default function Trabajadores() {
       <h2>Trabajadores</h2>
 
       {loading && <p>Cargando trabajadores...</p>}
-      {error && <p style={{ color: 'red' }}>{error}</p>}
-      {success && <p style={{ color: 'green' }}>{success}</p>}
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      {success && <p style={{ color: "green" }}>{success}</p>}
 
       <div className="columnas-layout">
         <section className="columna-lista-trabajadores">
           <ul className="lista-trabajadores">
             {trabajadores.map((trabajadores) => (
               <li key={trabajadores.id}
-              className={`tarjeta-trabajador ${selectedId === trabajadores.id ? 'seleccionado' : ''}`}
+              className={`tarjeta-trabajador ${selectedId === trabajadores.id ? "seleccionado" : ""}`}
               onClick={() => setSelectedId(trabajadores.id)}
               >
                 <p>{trabajadores.nombreCompleto}</p>
@@ -42,22 +42,92 @@ export default function Trabajadores() {
 
           {!loadingDetalle && detalle ? (
             <div className="ficha-trabajador">
+              <div className="contenedor-foto-perfil" 
+                    style={{ 
+                            textAlign: "center", 
+                            marginBottom: "15px" }}>
+
+                {detalle.foto_url ? (
+                  <img 
+                    src={detalle.foto_url} 
+                    alt={`Foto de ${detalle.nombreCompleto}`} 
+                    style={{ 
+                            width: "120px", 
+                            height: "120px", 
+                            borderRadius: "10%", 
+                            objectFit: "cover", 
+                            display: "inline-flex", 
+                            alignItems: "center", 
+                            justifyContent: "center", 
+                            border: "2px solid #ccc" }}
+                  />
+                ) : (
+                  <div style={{ 
+                            width: "120px", 
+                            height: "120px", 
+                            borderRadius: "10%", 
+                            backgroundColor: "#eee", 
+                            display: "inline-flex", 
+                            alignItems: "center", 
+                            justifyContent: "center", 
+                            color: "#666" }}>
+                    Sin Foto
+                  </div>
+                )}
+            </div>
               <p>Nombre: {detalle.nombreCompleto}</p>
-              <p>Rol: {detalle.rol}</p>
               <p>Rut: {detalle.rut}</p>
-            
+              <p>Sexo: {detalle.sexo}</p>
+              <p>Rol actual: {detalle.rol}</p>
+              <p>Fecha de nacimiento: {detalle.nacimiento} </p>
+              <p>Fecha de Contratación: {detalle.createdAt}</p>
+              <div className="campo-editable">
+                <label>Competencias:</label>
+                <input type="text" defaultValue={detalle.competencias || "Sin Competencias registradas"}/>
+              </div>
               <div className="campo-editable">
                 <label>Grupo asignado:</label>
                 <input type="text" defaultValue={detalle.grupo || "Sin Grupo"}/>
               </div>
+              <div className="documentosAdjuntos"
+                    style={{ 
+                      marginTop: '20px', 
+                      padding: '10px', 
+                      background: '#f9f9f9', 
+                      borderRadius: '5px' }}>
 
-              <div className="campo-editable">
-                <label>Antecedentes</label>
-                <textarea defaultValue={detalle.antecedentes || ""} />
-              </div>
+                <h4>Documentos Adjuntos</h4>
+                  <div style={{
+                    marginBottom: "10px"
+                  }}>
+                    <label style={{ 
+                                  fontWeight: 'bold', 
+                                  display: 'block' }}> 
+                      Curriculum Vitae 
+                    </label>
+                    <a href= {detalle.cv_url} 
+                      target="_blank" 
+                      rel="noopener noreferrer"
+                      className="btn-documento">Ver o descargar</a>
+                  </div>
+              
+                  <div style={{
+                    marginBottom: "10px"
+                  }}>
+                    <label style={{ 
+                                  fontWeight: 'bold', 
+                                  display: 'block' }}> 
+                      Antecedentes 
+                    </label>
+                    <a href= {detalle.antecedentes_url} 
+                      target="_blank" 
+                      rel="noopener noreferrer"
+                      className="btn-documento">Ver o descargar</a>
+                  </div>
+                </div>
+          
             
-
-            {detalle.historialDesvinculaciones && (
+            {/*detalle.historialDesvinculaciones && (
               <div className="historial.box">
               <h4>Historial Desvinculaciones:</h4>
               {detalle.historialDesvinculaciones.length === 0 ? (
@@ -70,7 +140,7 @@ export default function Trabajadores() {
                 </ul>
               )}
               </div>
-            )}
+            )}*/}
           </div>
           ): (
             !loadingDetalle && <p className="sin-seleccion">Haz clic en un trabajador para ver los detalles</p>
