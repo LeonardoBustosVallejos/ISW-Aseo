7df383d4df4af7ffa523bfffe0aa7ea056780d49
Gabo288 <gabriel.jerez2201@alumnos.ubiobio.cl>
2026-07-07 21:16:08 -0400

    Restaurar ruta accidentalmente borrada en el último merge

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import "../styles/sidebar.css"
 import { logout } from "../services/auth.service.js";
-import { BookUser, CircleChevronDown, FileUser, House, Info, LogOut, Network, NotebookPen, PanelLeftClose, PanelLeftOpen, ShelvingUnit, TableProperties, Trash2, UserRound, UserRoundCheck, UserRoundPlus, UsersRound, Warehouse } from "lucide-react";
+import { BookUser, CircleChevronDown, FileText, FileUser, House, Info, LogOut, Network, NotebookPen, PanelLeftClose, PanelLeftOpen, ShelvingUnit, TableProperties, Trash2, UserRound, UserRoundCheck, UserRoundPlus, UsersRound, Warehouse } from "lucide-react";
 
 const Sidebar = ({ isOpen, setIsOpen }) => {
     const navigate = useNavigate();
@@ -58,6 +58,7 @@ const Sidebar = ({ isOpen, setIsOpen }) => {
             title: "Clientes",
             children: [
                 { symbol: <UsersRound />, title: "Lista de Clientes", path: "/clientes" },
+                { symbol: <FileText />, title: 'Documentos Comerciales', path: '/clientes/documentos+comerciales' },
                 { symbol: <UserRoundPlus />, title: "Agregar", path: "/clientes/registrar" },
             ],
 
